727dd686f049d2f8c7a4d668e08fcaa8e89fcf84
zigur <massimorgon@gmail.com>
2015-11-23 16:28:39 +0100

    uniform interface for module

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 let PgCrudManager = require('./lib/crud/PgCrudManager');
 let PgQueryBuilder = require('./lib/query/PgQueryBuilder');
-module.exports.PgCrudManager = PgCrudManager;
-module.exports.PgQueryBuilder = PgQueryBuilder;
+module.exports.CrudManager = PgCrudManager;
+module.exports.QueryBuilder = PgQueryBuilder;
