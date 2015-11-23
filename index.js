9c19d4aea233813d9522e41ce2033d9b4bf2efe5
zigur <massimorgon@gmail.com>
2015-11-23 16:48:12 +0100

    uniform interface for module #2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* jshint esnext: true */
+/* jshint node: true */
+"use strict";
 
 let PgCrudManager = require('./lib/crud/PgCrudManager');
 let PgQueryBuilder = require('./lib/query/PgQueryBuilder');
